CharString/django

commit 6902824ac204d2207f50a3b788ae9d3d5947d01f

Fixed #11707 - limit_choices_to on a ForeignKey can render duplicate options in formfield
Thanks to Chris Wesseling for the report and patch.

git-svn-id: http://code.djangoproject.com/svn/django/trunk@15607 bcc190cf-cafb-0310-a4f2-bffc1f526a37

tests/regressiontests/model_fields/models.py
@@ -29,6 +29,11 @@ class Bar(models.Model):
     b = models.CharField(max_length=10)
     a = models.ForeignKey(Foo, default=get_foo)
 
+class Baz(models.Model):
+    a = models.CharField(max_length=5)
+    #Only Foos related to Bars starting with 'a'
+    foo = models.ForeignKey(Foo, limit_choices_to=models.Q(bar__b__startswith='a'))
+
 class Whiz(models.Model):
     CHOICES = (
         ('Group 1', (

tests/regressiontests/model_fields/tests.py
@@ -1,5 +1,6 @@
 import datetime
 from decimal import Decimal
+import re
 
 from django import test
 from django import forms
@@ -8,7 +9,7 @@ from django.db import models
 from django.db.models.fields.files import FieldFile
 from django.utils import unittest
 
-from models import Foo, Bar, Whiz, BigD, BigS, Image, BigInt, Post, NullBooleanModel, BooleanModel, Document
+from models import Foo, Bar, Baz, Whiz, BigD, BigS, Image, BigInt, Post, NullBooleanModel, BooleanModel, Document
 
 # If PIL available, do these tests.
 if Image:
@@ -95,12 +96,28 @@ class DecimalFieldTests(test.TestCase):
         # This should not crash. That counts as a win for our purposes.
         Foo.objects.filter(d__gte=100000000000)
 
+class BazForm(forms.ModelForm):
+    class Meta:
+        model = Baz
+
 class ForeignKeyTests(test.TestCase):
     def test_callable_default(self):
         """Test the use of a lazy callable for ForeignKey.default"""
         a = Foo.objects.create(id=1, a='abc', d=Decimal("12.34"))
         b = Bar.objects.create(b="bcd")
         self.assertEqual(b.a, a)
+
+    def test_distinct_choice_limit(self):
+        """Doesn't make sense to offer the same ForeignKey multiple times in a form"""
+        a = Foo.objects.create(a='a', d=Decimal("-1"))
+        b = Foo.objects.create(a='b', d=Decimal("1"))
+        bar_a = Bar.objects.create(b='ah', a=a)
+        bar_b = Bar.objects.create(b='aha', a=a)
+        bar_b = Bar.objects.create(b='bla', a=b)
+        form = BazForm()
+        fk_field = str(form['foo'])
+        self.assertEqual(len(re.findall(r'value="2"', fk_field)), 0)
+        self.assertEqual(len(re.findall(r'value="1"', fk_field)), 1)
 
 class DateTimeFieldTests(unittest.TestCase):
     def test_datetimefield_to_python_usecs(self):

django/db/models/fields/related.py
@@ -910,7 +910,7 @@ class ForeignKey(RelatedField, Field):
         db = kwargs.pop('using', None)
         defaults = {
             'form_class': forms.ModelChoiceField,
-            'queryset': self.rel.to._default_manager.using(db).complex_filter(self.rel.limit_choices_to),
+            'queryset': self.rel.to._default_manager.using(db).complex_filter(self.rel.limit_choices_to).distinct(),
             'to_field_name': self.rel.field_name,
         }
         defaults.update(kwargs)

commit 67b955f1cd75b84cfe42c00fb1c88da1656b893b

Fixed #11707 for default SQLite.
Needs testcase for the Postgres Point case that broke with
6902824ac204d2207f50a3b788ae9d3d5947d01f

django/forms/models.py
@@ -1064,12 +1064,12 @@ class ModelChoiceIterator(object):
         if self.field.cache_choices:
             if self.field.choice_cache is None:
                 self.field.choice_cache = [
-                    self.choice(obj) for obj in self.queryset.all()
+                    self.choice(obj) for obj in self.queryset.annotate()
                 ]
             for choice in self.field.choice_cache:
                 yield choice
         else:
-            for obj in self.queryset.all():
+            for obj in self.queryset.annotate():
                 yield self.choice(obj)
 
     def __len__(self):
@@ -1163,7 +1163,7 @@ class ModelChoiceField(ChoiceField):
             return None
         try:
             key = self.to_field_name or 'pk'
-            value = self.queryset.get(**{key: value})
+            value = self.queryset.annotate().get(**{key: value})
         except (ValueError, self.queryset.model.DoesNotExist):
             raise ValidationError(self.error_messages['invalid_choice'], code='invalid_choice')
         return value

tests/model_fields/models.py
@@ -28,6 +28,12 @@ class Bar(models.Model):
     a = models.ForeignKey(Foo, default=get_foo)
 
 
+class Baz(models.Model):
+    a = models.CharField(max_length=5)
+    #Only Foos related to Bars starting with 'a'
+    foo = models.ForeignKey(Foo, limit_choices_to=models.Q(bar__b__startswith='a'))
+
+
 class Whiz(models.Model):
     CHOICES = (
         ('Group 1', (

tests/model_fields/tests.py
@@ -4,6 +4,7 @@ import datetime
 from decimal import Decimal
 import unittest
 import warnings
+import re
 
 from django import test
 from django import forms
@@ -23,7 +24,7 @@ from django.utils.functional import lazy
 from .models import (
     Foo, Bar, Whiz, BigD, BigS, BigInt, Post, NullBooleanModel,
     BooleanModel, PrimaryKeyCharModel, DataModel, Document, RenamedField,
-    VerboseNameField, FksToBooleans, FkToChar)
+    VerboseNameField, FksToBooleans, FkToChar, Baz)
 
 
 class BasicFieldTests(test.TestCase):
@@ -139,6 +140,11 @@ class DecimalFieldTests(test.TestCase):
         Foo.objects.filter(d__gte=100000000000)
 
 
+class BazForm(forms.ModelForm):
+    class Meta:
+        model = Baz
+
+
 class ForeignKeyTests(test.TestCase):
     def test_callable_default(self):
         """Test the use of a lazy callable for ForeignKey.default"""
@@ -156,6 +162,30 @@ class ForeignKeyTests(test.TestCase):
         fk_model_empty = FkToChar.objects.create(out=char_model_empty)
         fk_model_empty = FkToChar.objects.select_related('out').get(id=fk_model_empty.pk)
         self.assertEqual(fk_model_empty.out, char_model_empty)
+
+    def test_distinct_choice_limit(self):
+        """Doesn't make sense to offer the same ForeignKey multiple times in a form"""
+        a = Foo.objects.create(a='a', d=Decimal("-1"))
+        b = Foo.objects.create(a='b', d=Decimal("1"))
+        Bar.objects.create(b='ah', a=a)
+        Bar.objects.create(b='aha', a=a)
+        Bar.objects.create(b='bla', a=b)
+        form = BazForm()
+        fk_field = str(form['foo'])
+        self.assertEqual(len(re.findall(r'value="%s"' % b.pk, fk_field)), 0)
+        self.assertEqual(len(re.findall(r'value="%s"' % a.pk, fk_field)), 1)
+
+    def test_distinct_choice_limit_save(self):
+        """Doesn't make sense to offer the same ForeignKey multiple times in a form"""
+        a = Foo.objects.create(a='a', d=Decimal("-1"))
+        b = Foo.objects.create(a='b', d=Decimal("1"))
+        Bar.objects.create(b='ah', a=a)
+        Bar.objects.create(b='aha', a=a)
+        Bar.objects.create(b='bla', a=b)
+        form = BazForm({'foo': a.pk, 'a': 'a'})
+        self.assertTrue(form.is_valid())
+        obj = form.save()
+        self.assertEqual(obj.foo.pk, a.pk)
 
 
 class DateTimeFieldTests(unittest.TestCase):